208e9039b8f7f9f0d6da4d596eb16a305d35dda8
Niraj Gandhi <gandhinira@myvuw.ac.nz>
2023-10-10 22:12:22 +1300

    implement Persistency

--- src/main/java/nz/ac/wgtn/swen301/restappender/server/Persistency.java
+++ src/main/java/nz/ac/wgtn/swen301/restappender/server/Persistency.java
@@ -1,12 +1,10 @@
 package nz.ac.wgtn.swen301.restappender.server;
 
-
-
-import org.apache.logging.log4j.core.LogEvent;
+import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Persistency {
-    public static List<LogEvent> DB = new ArrayList<>();
+    public static List<JsonObject> DB = new ArrayList<>();
 }
